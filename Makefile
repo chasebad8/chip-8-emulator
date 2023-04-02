33a510bbf62466dfe525519ba1e38a0dbf6fea8b
Chase B <chasebad8@hotmail.com>
2023-04-02 19:27:23 -0400

    More changes.

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@ SRC_DIR = src
 INC_DIR = include
 OBJ_DIR = obj
 LIB_DIR = libs
-SPDLOG_PATH=./libs/spdlog/build
+SPDLOG_PATH=./$(LIB_DIR)/spdlog/build
 
 # Compiler flags
-CXXFLAGS = -c -Wall -g
-INCLUDES = -I$(INC_DIR) -I./libs/spdlog/include/
-LDFLAGS = -L$(SPDLOG_PATH)/spdlog
+CXXFLAGS = -c -Wall -g `sdl2-config --cflags --libs`
+INCLUDES = -I$(INC_DIR) -I./libs/spdlog/include/ -I/usr/include/
+LDFLAGS = -L$(SPDLOG_PATH) -lspdlog
 
 # Source files
 SRCS = $(wildcard $(SRC_DIR)/*.cpp)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "cpu.h"
 #include "opcodes.h"
+#include <SDL2/SDL.h>
 
 #define SPDLOG_DEBUG_ON
 
@@ -37,6 +38,37 @@ int main()
    log_file_init();
    std::shared_ptr<spdlog::logger> logger = spdlog::get("main");
    logger->info("Booting up Chip-8 ...");
+
+   if(SDL_Init(SDL_INIT_VIDEO) < 0)
+   {
+      std::cout << "Failed to initialize the SDL2 library\n";
+      return -1;
+   }
+
+   SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       680, 480,
+                                       0);
+
+   if(!window)
+   {
+      std::cout << "Failed to create window\n";
+      return -1;
+   }
+
+   SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+   if(!window_surface)
+   {
+      std::cout << "Failed to get the surface from the window\n";
+      return -1;
+   }
+
+   SDL_UpdateWindowSurface(window);
+
+   SDL_Delay(5000);
+   
    CPU cpu;
 
    cpu.run();
